66f95e29782cbd617ffe9a99b2c9b1acffbdb3a3
Hans-Georg Sommer <hans-georg.sommer@gwdg.de>
2019-05-14 09:26:57 +0000

    Delete small example scripts

--- script-calendar.py
+++
@@ -1,9 +0,0 @@
-# Python program to display calendar of given month of the year
-
-import calendar
-
-# Ask month and year from the user
-yy = int(input("Enter year: "))
-mm = int(input("Enter month: "))
-
-print(calendar.month(yy, mm))
